f74ce1b81a434641a7a18fa16128c38a7bf60dca
Huxley <huxley.spark@gmail.com>
2010-10-16 02:52:20 -0700

    Working on upload

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,2 +1,12 @@
 class DataFile < ActiveRecord::Base
+	def self.save(upload)
+		name = upload['datafile'].original_filename
+		directory = "public/data"
+
+		# Create the file path
+		path = file.join(directory, name)
+
+		# Write the file
+		File.open(path, "wb") {|f| f.write(upload['datafile'].read) }
+	end
 end
