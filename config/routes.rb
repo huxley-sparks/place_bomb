f2bb4b71061bcfb9d8247dfc538f3a80b7ac8cd7
Huxley <huxley.spark@gmail.com>
2010-10-16 03:21:30 -0700

    modifying upload

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ PlaceDrop::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match 'upload/index'	=> 'upload#index'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
--- db/migrate/20101016054314_upload.rb
+++
@@ -1,7 +0,0 @@
-class Upload < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
